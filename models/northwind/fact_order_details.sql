13be0c166c3fc7debc6e6b46d30d6ca7033a89b5
Amulya Walimbe <awalimbe@g.syr.edu>
2025-11-25 02:17:27 +0000

    Assignment F completion

--- models/northwind/fact_order_details.sql
+++ models/northwind/fact_order_details.sql
@@ -1,15 +1,11 @@
 with od as (
-
     select *
     from {{ source('northwind', 'Order_Details') }}
-
 ),
 
 orders as (
-
     select *
     from {{ source('northwind', 'Orders') }}
-
 )
 
 select
@@ -19,10 +15,12 @@ select
     od.Quantity as quantity,
     od.Discount as discount,
     (od.UnitPrice * od.Quantity) * (1 - od.Discount) as total_price,
+
     orders.CustomerID as customerid,
     orders.EmployeeID as employeeid,
     orders.ShipVia as shipperid,
     orders.OrderDate as orderdate
+
 from od
 left join orders
     on od.OrderID = orders.OrderID
